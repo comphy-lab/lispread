82784912a690a2fbe8fea7a2ad7822e564b4c5e3
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2025-12-12 16:24:48 +0100

    mpi

--- bash_scripts/run_sweep_mpi.sh
+++ bash_scripts/run_sweep_mpi.sh
@@ -98,7 +98,7 @@ hf_${hf}_Ldomain_${Ldomain}_delta_${delta}"
 
     # Each simulation is a Slurm step that uses exactly RANKS_PER_SIM tasks
 
-    srun -n "${THREADS_PER_SIM}" \
+    mpirun -np "${THREADS_PER_SIM}" \
       ./bubbleAtLubis_mpi \
       "$Ohd" "$Ohf" "$Ohe" "$rhod" "$rhof" "$rhoe" \
       "$sigma_1" "$sigma_2" "$hf" "$tmax" "$Ldomain" "$delta" "$MAXlevel" "$savefolder" \

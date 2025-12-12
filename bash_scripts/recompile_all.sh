30d99afc7b06594827cb5f7ff2628483bff335b2
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2025-12-12 13:53:58 +0100

    mpi threading

--- bash_scripts/recompile_all.sh
+++ bash_scripts/recompile_all.sh
@@ -28,3 +28,4 @@ qcc -Wall -O2 getFacet1.c -o getFacet1 -lm -disable-dimensions
 qcc -Wall -O2 getFacet2.c -o getFacet2 -lm -disable-dimensions
 qcc -Wall -O2 getData.c  -o getData  -lm -disable-dimensions
 qcc -Wall -O2 getX0Y0V0.c -o getX0Y0V0 -lm -disable-dimensions
+CC99='mpicc -std=c99' qcc -Wall -O2 -D_MPI=1 bubbleAtLubis.c -o bubbleAtLubis_mpi -lm -disable-dimensions

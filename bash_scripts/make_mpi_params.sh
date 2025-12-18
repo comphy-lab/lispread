05adc8cd2969ab95bdd206054300853de4c90482
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2025-12-18 14:43:34 +0100

    2500_to_2515 maxLevel12

---
+++ bash_scripts/make_mpi_params.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+Ohe_list=( "5.0e-3" "2.5e-2" )
+Ohf_list=( "10" "20" "30" "40" )
+tmax_list=( "1" "1.5" "2.5" "2.5" )
+Ohd_list=( "9.1e-5" )
+sigma2_list=( "0.33" )
+sigma1_list=( "0.67" )
+MAXlevel_list=( "10" )
+hf_list=( "0.03" "0.05" )
+
+id_start="2500"
+id_end="2515"
+out="bash_scripts/mpi_params/params_${id_start}_to_${id_end}.txt"
+
+: > "$out"
+
+runid=$((id_start))   # starting run ID
+
+for MAXlevel in "${MAXlevel_list[@]}"; do
+  for i in "${!Ohf_list[@]}"; do
+    Ohf="${Ohf_list[$i]}"
+    tmax="${tmax_list[$i]}"
+    for Ohd in "${Ohd_list[@]}"; do
+      for hf in "${hf_list[@]}"; do
+        for Ohe in "${Ohe_list[@]}"; do
+          for sigma_1 in "${sigma1_list[@]}"; do
+            for sigma_2 in "${sigma2_list[@]}"; do
+
+              printf "%d %s %s %s %s %s %s %s %s\n" \
+                "$runid" \
+                "$Ohe" "$Ohf" "$tmax" "$Ohd" \
+                "$sigma2" "$sigma1" "$MAXlevel" "$hf" >> "$out"
+
+              runid=$((runid+1))
+            done
+          done
+        done
+      done
+    done
+  done
+done
+
+
+if [ "$runid" -ne $((id_end + 1)) ]; then
+  echo "ERROR: runid ended at $((runid-1)), expected $id_end" >&2
+  exit 1
+fi
---
+++ bash_scripts/properties/2500_to_2515_MaxLevel12/2501_properties.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#############################
+# Run ID  #
+#############################
+id_start="2500"
+id_end="2515"
+sub_save_folder="${id_start}_to_${id_end}_MaxLevel12/"
+id_start=2501
+
+###################################
+# Simulation and sweep parameters #
+###################################
+
+# Base parameters (shared across runs)
+rhoe="1"
+rhof="0.9"
+rhod="1.2e-3"
+Ldomain="3"
+delta="0.01"
+
+# Parameter sweeps
+Ohe_list=( "2.5e-2")
+Ohf_list=( "10" )
+tmax_list=( "20" )
+Ohd_list=( "9.1e-5" )
+sigma2_list=( "0.33" )
+sigma1_list=( "0.67" )
+MAXlevel_list=( "12" )
+hf_list=( "0.03" )
+
+# Concurrency control
+MAX_PAR=1         # how many sims to run at once
+THREADS_PER_SIM=32 # OpenMP/MPI threads per sim
+
+
+#############################
+# SBATCH related parameters #
+#############################
+
+SBATCH_JOB_NAME="${id_start}_MaxLevel12"
+SBATCH_TIME="96:00:00"
+
+SBATCH_NODES=1
+SBATCH_NTASKS=$(( MAX_PAR * THREADS_PER_SIM))
+SBATCH_NTASKS_PER_CORE=1
+SBATCH_CPUS_PER_TASK=1
+

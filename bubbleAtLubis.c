992fb28e3d6689b6af7390c51f03ae6c3ce3bc33
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-05 16:45:45 +0100

    removed usum

--- bubbleAtLubis.c
+++ bubbleAtLubis.c
@@ -232,12 +232,12 @@ event logWriting (i+=5) {
     if (i == 0) {
       // fprintf (ferr, "i dt t ke\n");
       fp = fopen (logfile, "w");
-      fprintf (fp, "i dt t ke usum\n");
-      fprintf (fp, "%d %g %g %g %g\n", i, dt, t, ke, usum);
+      fprintf (fp, "i dt t ke \n");
+      fprintf (fp, "%d %g %g %g %g\n", i, dt, t, ke);
       fclose(fp);
     } else {
       fp = fopen (logfile, "a");
-      fprintf (fp, "%d %g %g %g %g\n", i, dt, t, ke, usum);
+      fprintf (fp, "%d %g %g %g %g\n", i, dt, t, ke);
       fclose(fp);
     }
     // fprintf (ferr, "%d %g %g %g\n", i, dt, t, ke);

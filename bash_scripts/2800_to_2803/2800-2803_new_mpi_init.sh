2240bb14178b980c4b5fe0a95f6a3247a876de9e
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-20 13:36:09 +0100

    testing of tripping droplets

---
+++ bash_scripts/2800_to_2803/2800-2803_new_mpi_init.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Base parameters (shared across runs)
+rhoe="1"
+rhof="0.9"
+rhod="1.2e-3"
+Ldomain="5"
+delta="0.01"
+
+# Parameter sweeps
+Ohe_list=( "5e-3" )
+Ohf_list=( "0.5" "5" )
+tmax_list=( "0.1" "0.1" )
+Ohd_list=( "9.1e-5" )
+sigma2_list=( "0.33" )
+sigma1_list=( "0.67" )
+MAXlevel_list=( "11" )
+hf_list=("0.05" "0.03")
+
+id_start="2800"
+id_end="2803"
+
+sims_simultaneously=$((id_end - id_start + 1))
+
+# Concurrency control
+SBATCH_CPUS_PER_TASK=16 # OpenMP threads per sim
+SBATCH_NTASKS=1 # MPI threads per sim
+export OMP_NUM_THREADS=16
+
+# Node shape
+SBATCH_NODES=1
+SBATCH_NTASKS_PER_CORE=1
+
+#############################
+# SBATCH related parameters #
+#############################
+
+SBATCH_JOB_NAME="2800_to_2803"
+SBATCH_ARRAY=2800-2803
+SBATCH_TIME="96:00:00"
+
+# base save directory
+base_save_dir="Results/${SBATCH_ARRAY}_new_mpi"
+PARAMS_FILE="bash_scripts/2800_to_2803/params_list_${SBATCH_ARRAY}_new_mpi.txt"
+EXE="./bubbleAtLubis"
+SBATCH_MEM="251G"

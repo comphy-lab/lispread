cd1ef7a38c726dde30b5d8a14ede389294990ff2
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-24 20:48:23 +0100

    redo sweep water on LIS

--- bash_scripts/3000_to_3161/3090-3115.sh
+++ bash_scripts/3000_to_3161/3090-3115.sh
@@ -37,10 +37,10 @@ SBATCH_NTASKS_PER_CORE=1
 
 SBATCH_JOB_NAME="3090_to_3125"
 SBATCH_ARRAY="3090-3125"
-SBATCH_TIME="24:00:00"
+SBATCH_TIME="96:00:00"
 
 # base save directory
 base_save_dir="Results/${SBATCH_ARRAY}"
 PARAMS_FILE="bash_scripts/3000_to_3161/params_list_${SBATCH_ARRAY}.txt"
 EXE="./bubbleAtLubis_unaltered_3phase"
-SBATCH_MEM="16G"
+SBATCH_MEM="251G"

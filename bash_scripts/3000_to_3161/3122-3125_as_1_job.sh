9d3d6e86477778977b6d47a3b5117a7e0189592e
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-24 18:17:42 +0100

    redo sweep water on LIS

--- bash_scripts/3000_to_3161/3122-3125_as_1_job.sh
+++ bash_scripts/3000_to_3161/3122-3125_as_1_job.sh
@@ -8,37 +8,37 @@ Ldomain="5"
 delta="0.01"
 
 # Parameter sweeps
-Ohd_list=( "1e-2" "5e-3" "2.5e-3" "1e-3" )
+Ohd_list=( "5e-3" )
 Ohf_list=( "0.01" "0.05" "0.1" "0.5" "1" "2.5" "5" "7.5" "10")
 tmax_list=( "3" "3" "3" "4" "5" "10" "10" "12" "12")
-Ohe_list=( "9.1e-5" )
+Ohe_list=( "5e-5" "8e-5" "1.2e-3" "1.5e-5")
 sigma1_list=( "0.33" )
 sigma2_list=( "0.67" )
 MAXlevel_list=("12")
 hf_list=("0.05")
 
-id_start="3126"
-id_end="3161"
+id_start="3122"
+id_end="3125"
 
 sims_simultaneously=$((id_end - id_start + 1))
 
 # Concurrency control
 SBATCH_CPUS_PER_TASK=252 # OpenMP threads per sim
 SBATCH_NTASKS=1 # MPI threads per sim
-export OMP_NUM_THREADS=6
-omp_num_threads=6
+export OMP_NUM_THREADS=16
+omp_num_threads=16
 
 # Node shape
 SBATCH_NODES=1
-SBATCH_NTASKS_PER_CORE=2
+SBATCH_NTASKS_PER_CORE=1
 
 #############################
 # SBATCH related parameters #
 #############################
 
-SBATCH_JOB_NAME="3126_to_3161"
-SBATCH_ARRAY="3126-3161"
-SBATCH_TIME="96:00:00"
+SBATCH_JOB_NAME="3090_to_3125"
+SBATCH_ARRAY="3090-3125"
+SBATCH_TIME="48:00:00"
 
 # base save directory
 base_save_dir="Results/${SBATCH_ARRAY}"
---
+++ bash_scripts/3000_to_3161/3126-3141_as_1_job.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Base parameters (shared across runs)
+rhoe="1"
+rhof="0.9"
+rhod="1.2e-3"
+Ldomain="5"
+delta="0.01"
+
+# Parameter sweeps
+Ohd_list=( "1e-2" "5e-3" "2.5e-3" "1e-3" )
+Ohf_list=( "0.01" "0.05" "0.1" "0.5" "1" "2.5" "5" "7.5" "10")
+tmax_list=( "3" "3" "3" "4" "5" "10" "10" "12" "12")
+Ohe_list=( "9.1e-5" )
+sigma1_list=( "0.33" )
+sigma2_list=( "0.67" )
+MAXlevel_list=("12")
+hf_list=("0.05")
+
+id_start="3126"
+id_end="3141"
+
+sims_simultaneously=$((id_end - id_start + 1))
+
+# Concurrency control
+SBATCH_CPUS_PER_TASK=252 # OpenMP threads per sim
+SBATCH_NTASKS=1 # MPI threads per sim
+export OMP_NUM_THREADS=16
+omp_num_threads=16
+
+# Node shape
+SBATCH_NODES=1
+SBATCH_NTASKS_PER_CORE=2
+
+#############################
+# SBATCH related parameters #
+#############################
+
+SBATCH_JOB_NAME="3126_to_3161"
+SBATCH_ARRAY="3126-3161"
+SBATCH_TIME="96:00:00"
+
+# base save directory
+base_save_dir="Results/${SBATCH_ARRAY}"
+PARAMS_FILE="bash_scripts/3000_to_3161/params_list_${SBATCH_ARRAY}.txt"
+EXE="./bubbleAtLubis_unaltered_3phase"
+SBATCH_MEM="251G"

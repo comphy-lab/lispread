d4eda7ce89572bef18fbc228ff5c9fe5c5b4569a
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-25 15:51:41 +0100

    as 1job v2

---
+++ bash_scripts/3000_to_3161/3000-3090_as_1_job_start/3030-3044_as_1_job_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+id_start="3015"
+id_end="3029"
+
+sims_simultaneously=$((id_end - id_start + 1))
+
+# Concurrency control
+SBATCH_CPUS_PER_TASK=252 # OpenMP threads per sim
+SBATCH_NTASKS=1 # MPI threads per sim
+export OMP_NUM_THREADS=16
+
+# Node shape
+SBATCH_NODES=1
+SBATCH_NTASKS_PER_CORE=1
+
+#############################
+# SBATCH related parameters #
+#############################
+
+SBATCH_JOB_NAME="3000_to_3089"
+SBATCH_ARRAY="3000-3089"
+SBATCH_TIME="96:00:00"
+
+# base save directory
+base_save_dir="Results/${SBATCH_ARRAY}"
+PARAMS_FILE="bash_scripts/3000_to_3161/params_list_${SBATCH_ARRAY}.txt"
+EXE="./bubbleAtLubis_unaltered_3phase"
+SBATCH_MEM="251G"

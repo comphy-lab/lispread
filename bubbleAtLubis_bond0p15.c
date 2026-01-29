f64f5db2ceb87583fb70fa07bc604a458689e9e2
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-01-29 17:16:41 +0100

    bond number 3phase

--- bubbleAtLubis_bond0p15.c
+++ bubbleAtLubis_bond0p15.c
@@ -16,7 +16,7 @@
 #include "tension.h"   // add surface tension
 #include "distance.h"   // some geometry
 #include "adapt_wavelet_limited_v2.h" // adaptive mesh refinement
-#include "reduced.h"  // reduced gravity
+#include "reduced_three-phase.h"  // reduced gravity
 // #include "end_run_on_no_movement.h" // ends run on no movement
 
 #define MINlevel 3                                              // maximum grid size, opposite of MAXlevel

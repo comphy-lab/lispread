6e78db52cf2b2aa53a7e9f9c8bc7cbb16209f66f
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-02-04 14:01:16 +0100

    3000-3089 updated

--- bash_scripts/3000_to_3161/3036-3071.sh
+++ bash_scripts/3000_to_3161/3036-3071.sh
@@ -10,22 +10,22 @@ delta="0.01"
 # Parameter sweeps
 Ohd_list=( "5e-3" )
 Ohf_list=( "0.01" "0.05" "0.1" "0.5" "1" "2.5" "5" "7.5" "10")
-tmax_list=( "3" "3" "3" "3" "3" "5" "8" "8" "8")
+tmax_list=( "3" "3" "3" "4" "5" "10" "10" "12" "12")
 Ohe_list=( "9.1e-5" )
 sigma1_list=( "0.33" )
 sigma2_list=( "0.67" )
-MAXlevel_list=( "12")
-hf_list=("0.05")
+MAXlevel_list=( "11")
+hf_list=("0.01" "0.03" "0.05" "0.1")
 
-id_start="3200"
-id_end="3208"
+id_start="3036"
+id_end="3071"
 
 sims_simultaneously=$((id_end - id_start + 1))
 
 # Concurrency control
-SBATCH_CPUS_PER_TASK=32 # OpenMP threads per sim
+SBATCH_CPUS_PER_TASK=16 # OpenMP threads per sim
 SBATCH_NTASKS=1 # MPI threads per sim
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=16
 
 # Node shape
 SBATCH_NODES=1
@@ -35,12 +35,12 @@ SBATCH_NTASKS_PER_CORE=1
 # SBATCH related parameters #
 #############################
 
-SBATCH_JOB_NAME="3200_to_3208"
-SBATCH_ARRAY="3200-3208"
+SBATCH_JOB_NAME="3036_to_3071"
+SBATCH_ARRAY="3036-3071"
 SBATCH_TIME="96:00:00"
 
 # base save directory
 base_save_dir="Results/${SBATCH_ARRAY}"
-PARAMS_FILE="bash_scripts/3200_to_3208/params_list_${SBATCH_ARRAY}_omp.txt"
-EXE="./bubbleAtLubis_bond0p15"
-SBATCH_MEM="251G"
+PARAMS_FILE="bash_scripts/3000_to_3161/params_list_${SBATCH_ARRAY}.txt"
+EXE="./bubbleAtLubis"
+SBATCH_MEM="16G"

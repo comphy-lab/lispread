425c93b10a08292fa1ab3a4210a6981579b9bd70
Mark Boulogne <m.c.boulogne@student.utwente.nl>
2026-02-12 23:58:59 +0100

    low Ohf water and bubble

--- bash_scripts/3416_to_3431/3416-3315.sh
+++ bash_scripts/3416_to_3431/3416-3315.sh
@@ -9,16 +9,16 @@ delta="0.01"
 
 # Parameter sweeps
 Ohf_list=("3e-3" "6e-3")
-Ohe_list=(  "9.1e-5")
+Ohd_list=(  "9.1e-5")
 tmax_list=( "3" "3" )
-Ohd_list=( 1e-3 1e-2)
-sigma1_list=( "0.33" )
-sigma2_list=( "0.67" )
+Ohe_list=( 1e-3 1e-2)
+sigma2_list=( "0.33" )
+sigma1_list=( "0.67" )
 MAXlevel_list=( "12")
 hf_list=("0.006" "0.015" "0.03" "0.1")
 
-id_start="3400"
-id_end="3415"
+id_start="3416"
+id_end="3431"
 
 sims_simultaneously=$((id_end - id_start + 1))
 
@@ -35,12 +35,12 @@ SBATCH_NTASKS_PER_CORE=1
 # SBATCH related parameters #
 #############################
 
-SBATCH_JOB_NAME="3400_to_3415"
-SBATCH_ARRAY="3400-3415"
+SBATCH_JOB_NAME="3416_to_3431"
+SBATCH_ARRAY="3416-3431"
 SBATCH_TIME="96:00:00"
 
 # base save directory
 base_save_dir="Results/${SBATCH_ARRAY}"
-PARAMS_FILE="bash_scripts/3400_to_3415/params_list_${SBATCH_ARRAY}.txt"
+PARAMS_FILE="bash_scripts/3416_to_3431/params_list_${SBATCH_ARRAY}.txt"
 EXE="./bubbleAtLubis"
 SBATCH_MEM="251G"
